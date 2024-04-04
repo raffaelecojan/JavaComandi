2236a83f4ab7ae05a315b69d2814685cc75c4ee1
raffaelecojan <165819080+raffaelecojan@users.noreply.github.com>
2024-04-04 18:47:49 +0200

    Add files via upload

---
+++ Eser/src/eser/Eser.java
@@ -0,0 +1,148 @@
+package eser;
+
+import java.util.Scanner;
+
+public class Eser {
+
+	public static void main(String[] args) {
+//		System.out.println("+ - - - - - + \n| raffaele  |\n+ - - - - - +");
+		
+		
+//		String parola1 = "Java", parola2 = "Script";
+//		System.out.println("la prima parola è lunga : "+ parola1.length()+" la seconda parola è lunga 2 "+parola2.length());
+//		System.out.println("parole unite "+parola1.concat(parola2));
+//		System.out.println("parole in maiuscolo "+ (parola1.concat(parola2)).toUpperCase());
+		
+		
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("inserisci un numero intero :");
+//		int aiuto = 10, k=0, cont=1;
+//		int n = input.nextInt();
+//		do {
+//			if(n<aiuto) {
+//				System.out.println("il numero ha "+cont+" cifre");
+//				k=1;
+//			}
+//			else {
+//				aiuto = aiuto*10;
+//				cont++;
+//			}			
+//		}while(k==0);
+		
+		
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("inserisci un numero intero :");
+//		int n = input.nextInt();
+//		int cont=0;
+//		for(int i=1; i<=n; i++) {
+//			if(n==1)
+//			System.out.println(n+" è divisibile per 1");
+//			else if(n%i==0) {
+//				cont++;
+//				System.out.println(n+" è divisibile per "+i);}	
+//		}
+//		System.out.println("i divisori sono "+cont);
+		
+		
+//		Scanner input = new Scanner(System.in);
+//		int n=0, max=0, min=100000000;
+//		do{
+//			System.out.println("inserisci un numero intero :");
+//			n = input.nextInt();
+//			if(n>max)
+//				max=n;
+//			if(n<min && n!=0)
+//				min=n;
+//		}while(n!=0);
+//		System.out.println("il minimo è "+min+" il massimo è "+max);
+		
+		
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("inserisci i tre lati del triangolo");
+//		int n1 = input.nextInt();
+//		int n2 = input.nextInt();
+//		int n3 = input.nextInt();
+//		if(n1-n2-n3>=0 && n2-n1-n3>=0 && n3-n1-n2>=0) 
+//			System.out.println("i triangolo esiste");
+//		else
+//			System.out.println("il triangolo non esiste");
+			
+		
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.println("inserisci una frase");
+//		String s = input.nextLine();
+//		System.out.println("inserisci un carattere");
+//		char c = input.next().charAt(0);
+//		char[] stringInCharArr = s.toCharArray();
+//		
+//		
+//		for(int i=0;i<s.length();i++) {
+//			if(stringInCharArr[i]==c) {
+//				System.out.println("la lettera "+c+" appartiene alla frase");}
+//		}
+		
+		
+//		Scanner input= new Scanner(System.in);
+//		System.out.println("inserisci un numero tra 1 e 7:");
+//		int n = input.nextInt();
+//		
+//		switch(n) {
+//		case 1: 
+//			System.out.println("lunedì");
+//			break;
+//		case 2: 
+//			System.out.println("martedì");
+//			break;
+//		case 3: 
+//			System.out.println("mercoledì");
+//			break;
+//		case 4: 
+//			System.out.println("giovedì");
+//			break;
+//		case 5: 
+//			System.out.println("venerdì");
+//			break;
+//		case 6: 
+//			System.out.println("sabato");
+//			break;
+//		case 7: 
+//			System.out.println("domenica");
+//			break;
+//		}
+		
+//	int n=0, somma=0;
+//	double media;
+//	Scanner input = new Scanner(System.in);
+//		for(int i=1;i<6;i++) {
+//			System.out.println("inserisci il "+i+" numero");
+//			n= input.nextInt();
+//			somma = somma+n;
+//		}
+//		media = (double)somma/5;
+//		System.out.println("la somma vale "+somma+" e la media vale "+media);
+		
+		
+//		Scanner input = new Scanner(System.in);
+//		System.out.println("inserisci un numero :");
+//		int n= input.nextInt();
+//		for(int i=1; i<=n; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+	
+		Scanner input = new Scanner(System.in);
+		System.out.println("inserisci un numero :");
+		int n= input.nextInt();
+		int cont=1;
+		for(int i=1;i<=n;i++) {
+			for(int j=1; j<=cont; j++) {
+				
+			}
+		}
+		
+	}
+
+}
